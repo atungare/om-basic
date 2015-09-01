0fa0bb920d20c3c5f5cf004ec42121bc04d4fa26
Ajay Tungare <ajay.tungare@gmail.com>
2015-08-31 22:07:13 -0700

    live coding; multiple om roots, apps

--- om-tut/src/om_tut/core.cljs
+++ om-tut/src/om_tut/core.cljs
@@ -8,15 +8,21 @@
 
 ;; define your app data so that it doesn't get over-written on reload
 
-(defonce app-state (atom {:text "Hello world!"}))
+(defonce app-state (atom {:text "Hello world, from clj!"}))
 
 (om/root
   (fn [data owner]
-    (reify om/IRender
-      (render [_]
-        (dom/h1 nil (:text data)))))
+    (om/component
+      (dom/h2 nil (:text data))))
   app-state
-  {:target (. js/document (getElementById "app"))})
+  {:target (. js/document (getElementById "app0"))})
+
+(om/root
+  (fn [data owner]
+    (om/component
+      (dom/h2 nil (:text data))))
+  app-state
+  {:target (. js/document (getElementById "app1"))})
 
 
 (defn on-js-reload []
